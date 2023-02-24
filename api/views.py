0fc2d149db9283d0439d5065fccc12846bd0fd6b
jagi113 <jaroslavgirovsky@gmail.com>
2023-02-24 06:48:52 +0100

    project-vote

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from api.serializers import ProjectSerializer
-from projects.models import Project
+from projects.models import Project, Review
+
 
 # return all the routes of our api
 @api_view(['GET'])
@@ -27,5 +29,27 @@ def getProjects(request):
 @api_view(['GET'])
 def getProject(request, pk):
     project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectVote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+    
+    #This will create or get that object
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        project=project,
+    )
+    
+    # This will create or update that object
+    review.value = data['value']
+    review.body = data['body']
+    review.save()
+    project.getVoteCount
+    
     serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
